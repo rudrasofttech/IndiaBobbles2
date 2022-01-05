d551e8952f775e2ec106250aed884640b2ca312d
Raj Kiran Singh <rajkiran.singh@rudrasofttech.com>
2022-01-05 10:00:11 +0530

    Product Tags Page in admin

---
+++ admin/Tags.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class Tags
+    Inherits AdminPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        UserIDHidden.Value = CurrentUser.ID
+    End Sub
+
+    Protected Sub SaveButton_Click(sender As Object, e As EventArgs) Handles SaveButton.Click
+        SqlDataSource1.Insert()
+        Response.Redirect("~/admin/tags.aspx")
+    End Sub
+End Class
